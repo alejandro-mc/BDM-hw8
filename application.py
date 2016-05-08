3969c2994a85fdfc1e183142dfc93f44e1f2d255
Cortina Morejon <amorejo00@cluster.cusp.nyu.edu>
2016-05-08 05:03:10 -0400

    Wrote Mapper to produce ((neighborhood, borough), 1) pairs.

---
+++ application.py
@@ -0,0 +1,60 @@
+##Imports
+from pyspark import SparkContext
+import rtree
+import geopandas as gpd
+import shapely.geometry as geom
+
+### yellow_tripdata_2011-05.csv
+
+### 0  vendor_id,pickup_datetime,dropoff_datetime,passenger_count,
+### 4  trip_distance,pickup_longitude,pickup_latitude,rate_code,
+### 8  store_and_fwd_flag,dropoff_longitude,dropoff_latitude,
+### 11 payment_type,fare_amount,surcharge,mta_tax,tip_amount,
+### 16 tolls_amount,total_amount
+
+
+def tripMapper(records):
+    import rtree
+    import geopandas as gpd
+    import shapely.geometry as geom
+    #first create index
+    index = rtree.Rtree()
+    hoods = gpd.read_file('neighborhoods.geojson')
+    for idx,geometry in enumerate(hoods.geometry):
+        index.insert(idx,geometry.bounds)
+    #for each record yield ((nei_idx,boro_idx), 1)
+    neiborhood_name = "Undefined"
+    boro_name = "Undefined"
+    for record in records:
+        list_record = record.split(',')
+        if  list_record[0] != 'vendor_id' and list_record[0]!='':
+            point_origin      = geom.Point(float(list_record[5]),float(list_record[6]))
+            point_destination = geom.Point(float(list_record[9]),float(list_record[10]))
+            #get origin neighborhood
+            matches = list(index.intersection((point_origin.x,point_origin.y)))
+            for ind in matches:
+                if any(map(lambda x: x.contains(point_origin), hoods.geometry[ind])):
+                    neiborhood_name = hoods.neighborhood[ind]
+            #get destination borough
+            matches = list(index.intersection((point_destination.x,point_destination.y)))
+            for ind in matches:
+                if any(map(lambda x: x.contains(point_destination), hoods.geometry[ind])):
+                    boro_name = hoods.borough[ind]
+            yield ((neiborhood_name,boro_name),1)
+
+
+
+if __name__=='__main__':
+    if len(sys.argv)<3:
+        print "Usage: <input files> <output path>"
+        sys.exit(-1)
+
+    sc = pyspark.SparkContext()
+
+    trips = sc.textFile(','.join(sys.argv[1:-1]))
+
+    output = trips \
+        .mapPartitions(tripMapper).take(50)
+    
+    output.saveAsTextFile(sys.argv[-1])
+
